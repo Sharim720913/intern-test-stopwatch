ba1bc631243c906214920f9a8b1b67457d468f4c
Sharim720913 <99999835+Sharim720913@users.noreply.github.com>
2022-09-15 06:48:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Stopwatch from "./Components/Stopwatch";
+
+const App = () => {
+  return (
+    <div>
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
